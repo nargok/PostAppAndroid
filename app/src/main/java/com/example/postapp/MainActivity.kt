e4e467c6365e6d748275314687990c576e166545
nargok <nargok@gmail.com>
2024-07-15 16:18:24 +0900

    delete unused import

--- app/src/main/java/com/example/postapp/MainActivity.kt
+++ app/src/main/java/com/example/postapp/MainActivity.kt
@@ -23,20 +23,16 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.postapp.application.viewmodel.PostViewModel
 import com.example.postapp.domain.model.PostModel
 import com.example.postapp.ui.theme.PostAppTheme
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.HiltAndroidApp
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
--- app/src/main/java/com/example/postapp/application/viewmodel/Post.kt
+++ app/src/main/java/com/example/postapp/application/viewmodel/Post.kt
@@ -2,18 +2,12 @@ package com.example.postapp.application.viewmodel
 
 import android.os.Build
 import androidx.annotation.RequiresApi
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.postapp.domain.model.PostModel
 import com.example.postapp.domain.repository.PostRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.asFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -22,14 +16,6 @@ class PostViewModel @Inject constructor(
     private val repository: PostRepository
 ) : ViewModel() {
     val items = repository.list()
-
-//    init {
-//        loadTodos()
-//    }
-//
-//    private fun loadTodos() {
-//        _items.value = repository.list()
-//    }
 
     @RequiresApi(Build.VERSION_CODES.O)
     fun addPost(text: String) {
